fc7766dc864848fd786b15718139c22a680a2ad2
Franco Victorio <fake@fake.com>
2015-08-16 21:31:09 -0300

    C++ solution to sumLines

--- sumLines/solution.cpp
+++ sumLines/solution.cpp
@@ -4,6 +4,28 @@
 
 using namespace std;
 
-int main() {
+string sum_line(const string& line) {
+    int sum = 0;
+    int x;
 
+    stringstream ss;
+    ss << line;
+
+    while (ss >> x) {
+        sum += x;
+    }
+
+    if (ss.eof()) {
+        return to_string(sum);
+    } else {
+        return "Wrong line";
+    }
+}
+
+int main() {
+    string line;
+
+    while (getline(cin, line)) {
+        cout << sum_line(line) << endl;
+    }
 }
